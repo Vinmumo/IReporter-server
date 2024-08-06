f91521a933e1191150cbb23a669bef3d6e6db284
dickson4954 <dicksonmurithi155@gmail.com>
2024-08-06 12:20:26 +0300

    added user authorization routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 from myapp import create_app
+from myapp.routes.auth_routes import auth_bp
 
 app = create_app()
+
+app.register_blueprint(auth_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
