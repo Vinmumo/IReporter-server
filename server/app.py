ac5f6640d514e29c13e90414aab013f02f5dbfec
l3xst1ng <howdy.martt@gmail.com>
2024-08-08 20:45:45 +0300

    Fix: Optimize the Flask application entry point      - Imported the create_app() function from the init.py file. - Defined the application configuration to run the Flask application in debug mode. - Removed blueprints

--- server/app.py
+++ server/app.py
@@ -1,17 +1,7 @@
 from myapp import create_app
-from myapp.routes.auth_routes import auth_bp
-from myapp.routes.image_routes import image_bp
-from myapp.routes.record_routes import record_bp
-from myapp.routes.video_routes import video_bp
 
 app = create_app()
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(image_bp)
-app.register_blueprint(record_bp)
-app.register_blueprint(video_bp)
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
